8b3baa154acd873e24d98a111aaf2dce02230133
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2015-06-04 15:31:31 -0700

    adding compress option (-c) to Uglify.

--- lib/smart_asset.rb
+++ lib/smart_asset.rb
@@ -11,15 +11,15 @@ require 'smart_asset/helper'
 
 class SmartAsset
   class <<self
-    
+
     attr_accessor :append_random, :asset_host, :asset_counter, :cache, :config, :dest, :env, :envs, :pub, :root, :sources
-    
+
     def binary(root, relative_config=nil)
       load_config root, relative_config
       compress 'javascripts'
       compress 'stylesheets'
     end
-    
+
     def compress(type)
       dest = @dest[type]
       dir = "#{@pub}/#{@sources[type]}"
@@ -30,9 +30,9 @@ class SmartAsset
       change = Change.new(dir)
       change.d
       states = change.send(:states)
-      
+
       FileUtils.mkdir_p dest
-      
+
       (@config[type] || {}).each do |package, files|
         next if ENV['PACKAGE'] && ENV['PACKAGE'] != package
         if files
@@ -45,46 +45,40 @@ class SmartAsset
             array
           end
           next if hashes.empty?
-          
+
           # Modified hash
           hash = Digest::SHA1.hexdigest(hashes.join)[0..7]
-          
+
           # Package path
           package = "#{dest}/#{hash}_#{package}.#{ext}"
-          
+
           # If package file exists
           if File.exists?(package)
             packages << package
           else
             data = []
-            
+
             # Join files in package
             files.each do |file|
               if File.exists?(source = "#{dir}/#{file}.#{ext}")
                 data << File.read(source)
               end
             end
-            
+
             # Don't create new compressed file if no data
             data = data.join("\n")
             next if data.strip.empty?
-            
+
             # Compress joined files
             tmp = "#{dest}/tmp.#{ext}"
             File.open(tmp, 'w') { |f| f.write(data) }
             puts "\nCreating #{package}..."
             warning = ENV['WARN'] ? " -v" : nil
-            
+
             @bin = File.expand_path(File.dirname(__FILE__) + "/../node_modules") + "/"
-              
-            # Ignore npm binary path for now
-            # unless @bin
-            #   @bin = Dir.chdir(@root) { `npm bin`.strip }
-            #   @bin = File.directory?(@bin) ? "#{@bin}/" : nil
-            # end
-            
+
             if ext == 'js'
-              cmd = "#{@bin}uglify-js/bin/uglifyjs --output #{package}#{warning} #{tmp}"
+              cmd = "#{@bin}uglify-js/bin/uglifyjs -c -o #{package}#{warning} #{tmp}"
             elsif ext == 'css'
               cmd = "#{@bin}clean-css/bin/cleancss --skip-advanced #{tmp} -o #{package}"
             end
@@ -93,9 +87,9 @@ class SmartAsset
             cmd_output = `#{cmd}`
 
             raise "Error running `#{cmd}`:\n#{cmd_output}"  unless $?.success?
-            
+
             FileUtils.rm(tmp) unless ENV['DEBUG']
-            
+
             # Fix YUI compression issue
             if ext == 'css'
               if RUBY_PLATFORM.downcase.include?('darwin')
@@ -104,24 +98,24 @@ class SmartAsset
                 `sed -i 's/ and(/ and (/g' #{package}`
               end
             end
-            
+
             # Package created
             packages << package
           end
         end
       end
-      
+
       # Remove old/unused packages
       (Dir["#{dest}/#{"[^_]"*8}_*.#{ext}"] - packages).each do |path|
         FileUtils.rm path
       end
-      
+
       # Delete legacy files
       Dir["#{dest}/*.yml", "#{dest}/#{"[0-9]"*14}_*.{css,js}"].each do |path|
         FileUtils.rm path
       end
     end
-    
+
     def load_config(root, relative_config=nil)
       relative_config ||= 'config/assets.yml'
       @root = File.expand_path(root)
@@ -130,7 +124,7 @@ class SmartAsset
 
       @cache = nil
       @config = YAML::load(File.read("#{@root}/#{relative_config}"))
-      
+
       # Default values
       if @config['append_random'].nil?
         @config['append_random'] = {}
@@ -138,20 +132,20 @@ class SmartAsset
       if @config['append_random'].is_a?(::Hash) && @config['append_random']['development'].nil?
         @config['append_random']['development'] = true
       end
-      
+
       @config['asset_host_count'] ||= 4
       @config['asset_host'] ||= ActionController::Base.asset_host rescue nil
       @config['environments'] ||= %w(production)
       @config['public'] ||= 'public'
-      
+
       @config['destination'] ||= {}
       @config['destination']['javascripts'] ||= 'javascripts/packaged'
       @config['destination']['stylesheets'] ||= 'stylesheets/packaged'
-      
+
       @config['sources'] ||= {}
       @config['sources']['javascripts'] ||= "javascripts"
       @config['sources']['stylesheets'] ||= "stylesheets"
-      
+
       # Convert from asset packager syntax
       %w(javascripts stylesheets).each do |type|
         if @config[type].respond_to?(:pop)
@@ -160,39 +154,39 @@ class SmartAsset
           end
         end
       end
-      
+
       # Class variables
-      @append_random = 
+      @append_random =
         if @config['append_random'].is_a?(::Hash)
           @config['append_random'][@env]
         else
           @config['append_random']
         end
-      
+
       @asset_host = @config['asset_host']
       @envs = @config['environments']
       @sources = @config['sources']
-      
+
       @pub = File.expand_path("#{@root}/#{@config['public']}")
       @dest = %w(javascripts stylesheets).inject({}) do |hash, type|
         hash[type] = "#{@pub}/#{@config['destination'][type]}"
         hash
       end
     end
-    
+
     def paths(type, match)
       match = match.to_s
-      
+
       @cache ||= {}
       @cache[type] ||= {}
-      
+
       if @cache[type][match]
         return @cache[type][match]
       end
-      
+
       dest = @dest[type]
       ext = ext_from_type type
-      
+
       if @envs.include?(@env.to_s)
         @cache[type][match] =
           if result = Dir["#{dest}/#{"[^_]"*8}_#{match}.#{ext}"].first
@@ -212,30 +206,30 @@ class SmartAsset
         result.flatten.compact.uniq
       end
     end
-    
+
     def prepend_asset_host(path)
       if @asset_host.respond_to?(:keys)
         host = @asset_host[@env.to_s]
       else
         host = @asset_host
       end
-      
-      if host    
+
+      if host
         if !@asset_counter || @asset_counter == @config['asset_host_count']
           @asset_counter = 0
         end
-      
+
         count = @asset_counter.to_s
         @asset_counter += 1
-      
+
         host.gsub('%d', count) + path
       else
         path
       end
     end
-    
+
     private
-    
+
     def ext_from_type(type)
       case type
       when 'javascripts' then
